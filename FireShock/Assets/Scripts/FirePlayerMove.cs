e06ee27e0382d161c4413f030bebd527c6ba8244
rohaid-bakh <rohaidb1999@gmail.com>
2021-06-13 04:10:40 -0400

    added collider boxes and tags

---
+++ FireShock/Assets/Scripts/FirePlayerMove.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlayerMove : MonoBehaviour
+{
+    // Movement, animator, velocity, and physics for the player
+    Rigidbody2D rb;
+    Vector3 moveSpeed;
+    Animator animate;
+
+    // Fireballs yum
+    //[Header("Fireballs")]
+    //public GameObject fireSpawn;
+    //public List<GameObject> fires = new List<GameObject>();
+    //private int firesMax = 3; // feel free to set whatever number you want
+    //public int firesNum;
+
+    // Header velocity input
+
+    [Header("Speed")]
+    public float speed;
+
+    // Header onGround input
+    [Header("Is the object on the ground?")]
+    public bool onGround;
+
+    // Jump header (List mJumps to 1)
+    [Header("Jump Setting")]
+    public int jumps;
+    public int mJumps;
+
+    // Animation header
+    [Header("Animation Setting")]
+    public bool Idle_Left;
+    public bool Idle_Right;
+    public bool Jump_Left;
+    public bool Jump_Right;
+    public bool Run_Left;
+    public bool Run_Right;
+
+    // Sounds header
+    //[Header("Sounds Setting")]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mJumps = jumps;
+        rb = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // horizontal float
+        float horizontal = 1;
+        // Moving the player using the A S D keys
+
+        // left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * horizontal *speed * Time.deltaTime;
+        }
+
+        // right 
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+        }
+
+        // Make the player jump using the W key
+
+        if (Input.GetKey(KeyCode.W) && jumps > 0)
+        {
+            Jump();
+        }
+
+        // Make the player use action using the G key
+        if (Input.GetKey(KeyCode.G))
+        {
+            //Action();
+        }
+
+        // Animation void here
+        AnimationUpdate();
+        // RigidBody move
+        rb.MovePosition(transform.position + moveSpeed * Time.deltaTime);
+        
+    }
+
+    // jump code (Y has been set to 8. Feel free to change)
+    public void Jump()
+    {
+        // Animation void here
+        //AnimationUpdate();
+        rb.velocity = new Vector3(0, 8, 0);
+        onGround = false;
+        jumps--;
+    }
+
+    // action code
+    //public void Action()
+    //{
+       
+    //}
+
+    // animation code
+    public void AnimationUpdate()
+    {
+        // flip to the left
+        if (moveSpeed.x < 0)
+        {
+            animate.SetBool("Run_Left", true);
+            animate.SetBool("Idle_Left", false);
+        }
+        else
+        {
+            animate.SetBool("Run_Left", false);
+            animate.SetBool("Idle_Left", true);
+
+        }
+        // flip to the right
+        if (moveSpeed.x > 0)
+        {
+            animate.SetBool("Run_Right", true);
+            animate.SetBool("Idle_Right", false);
+        }
+        else
+        {
+            animate.SetBool("Run_Right", false);
+            animate.SetBool("Idle_Right", true);
+        }
+        // jump to the left
+        if (moveSpeed.x < 0 && moveSpeed.y > 0)
+        {
+            animate.SetBool("Jump_Left", true);
+        }
+        else
+        {
+            animate.SetBool("Jump_Left", false);
+        }
+        // jump to the right
+        if (moveSpeed.x > 0 && moveSpeed.y > 0)
+        {
+            animate.SetBool("Jump_Right", true);
+        }
+        else
+        {
+            animate.SetBool("Jump_Right", false);
+        }
+    }
+    
+
+    // collision code
+    private void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.transform.CompareTag("Ground"))
+        {
+            moveSpeed.y = 0;
+            onGround = true;
+            jumps = mJumps;
+        }
+    }
+    private void OnCollisionExit2D (Collision2D col)
+    {
+        if (col.transform.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+
+}
---
+++ FireShock/Assets/Scripts/MovementController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//issues,  animation keeps looping
+public class MovementController : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    private Rigidbody2D rigidbody2d;
+    private CapsuleCollider2D collider;
+    private float animatorTime;
+    public float speed = 4f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float jumpVelocity = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = gameObject.GetComponent<CapsuleCollider2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animatorTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        IsGrounded();
+        IsDodging();
+        float horizontal = 1;
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        transform.position = position;
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            animator.SetBool("Jumping", true);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow) && IsGrounded()) {
+            animator.SetBool("Dodging", true);
+        }
+    }
+
+    public void Jump()
+    {
+        rigidbody2d.velocity = Vector2.up * jumpVelocity;
+    }
+    public void EndJump()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+    public void EndDodge()
+    {
+        animator.SetBool("Dodging", false);
+    }
+
+    public void EndAttack()
+    {
+        animator.SetBool("Attacking", false);
+    } 
+
+    //animator.IsInTransition is to not cut the animation while stopping
+    //aimatorTime is to check on what loop count the animation is on
+    //Have to put in .4f instead of 1 because it would overshoot
+
+    private void IsDodging(){
+     if (animator.GetCurrentAnimatorStateInfo(0).IsName("MC_Dodge") && 
+     ( animatorTime >= .4f && !animator.IsInTransition(0)))
+        {
+            EndDodge();
+        }
+    }
+
+
+
+    private bool IsGrounded()
+    {
+        Vector2 position = collider.transform.position;
+        Vector2 direction = Vector2.down;
+        //variable that needs to be changed if you change size of sprite
+        float distance = 1.5f;
+        Debug.DrawRay(position, direction, Color.red, Time.deltaTime, false);
+        //draws a line and checks to see if there's anything below
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            animator.SetBool("Grounded", true);
+            return true;
+        }
+        animator.SetBool("Grounded", false);
+        return false;
+    }
+}
